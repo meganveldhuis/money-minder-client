f515bafcb0d73b0e41b01db695228dcb15eb08b3
lovedatsnow <megan.veldhuis@gmail.com>
2025-02-02 18:16:29 -0700

    create partials, make defaults for font sizes and add colours

--- client-budget-app/src/App.jsx
+++ client-budget-app/src/App.jsx
@@ -1,3 +1,4 @@
+import "./styles/partials/_global.scss";
 import "./App.css";
 
 function App() {
@@ -5,6 +6,9 @@ function App() {
     <>
       <h1>MoneyMinder</h1>
       <h2>By Megan</h2>
+      <h3>H3</h3>
+      <h4>H4</h4>
+      <p>Paragraph</p>
     </>
   );
 }
